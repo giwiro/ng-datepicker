a2d18b2776f3d60d2d35ec7ed19784f78c342934
giwiro <giwirodavalos@gmail.com>
2018-01-02 16:14:55 -0500

    export all elements

--- core/public_api.d.ts
+++ core/public_api.d.ts
@@ -1,4 +1,6 @@
 export * from './service/date-picker.service';
+export * from './pipes/pad-day-number/pad-day-number.pipe';
+export * from './directives/date-picker-single/date-picker-single.directive';
 export * from './components/date-picker-single/date-picker-single.component';
 export * from './components/single-calendar/single-calendar.component';
 export * from './components/ranged-calendar/ranged-calendar.component';
